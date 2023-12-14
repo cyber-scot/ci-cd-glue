b80a9a5ade6c1de373492c106d2b90b6d6de2a5a
Craig Thacker <craigthackerx@gmail.com>
2023-12-14 22:01:26 +0000

    Update code

--- .jenkins/Run-Jenkins.ps1
+++ .jenkins/Run-Jenkins.ps1
@@ -5,7 +5,9 @@ param (
     [int]$InternalWebPort = 8080,
     [int]$ExternalAgentPort = 50000,
     [int]$InternalAgentPort = 50000,
-    [string]$VolumeMapping = "jenkins_home:/var/jenkins_home"
+    [string]$VolumeMapping = "jenkins_home:/var/jenkins_home",
+    [string]$PrimaryDNS = "8.8.8.8", # Default to Google's public DNS
+    [string]$SecondaryDNS = "8.8.4.4" # Default to Google's secondary public DNS
 )
 
 # Pull the latest Jenkins Docker image
@@ -17,6 +19,8 @@ $ContainerId = $(docker run -d `
 -p "${ExternalAgentPort}:${InternalAgentPort}" `
 --name $ContainerName `
 --volume $VolumeMapping `
+--dns $PrimaryDNS `
+--dns $SecondaryDNS `
 --privileged `
 $ImageName)
 
--- .jenkins/TerraformJenkinsfile.groovy
+++ .jenkins/TerraformJenkinsfile.groovy
@@ -6,41 +6,50 @@ pipeline {
     }
 
     parameters {
-        booleanParam(name: "TfInit", defaultValue: true, description: 'Terraform Init')
-        booleanParam(name: "TfPlan", defaultValue: true, description: 'Terraform Plan')
-        booleanParam(name: "TfApply", defaultValue: false, description: 'Terraform Apply')
-        booleanParam(name: "TfPlanDestroy", defaultValue: false, description: 'Terraform Plan Destroy')
-        booleanParam(name: "TfDestroy", defaultValue: false, description: 'Terraform Destroy')
+        booleanParam(name: 'TfInit', defaultValue: true, description: 'Terraform Init')
+        booleanParam(name: 'TfPlan', defaultValue: true, description: 'Terraform Plan')
+        booleanParam(name: 'TfApply', defaultValue: false, description: 'Terraform Apply')
+        booleanParam(name: 'TfPlanDestroy', defaultValue: false, description: 'Terraform Plan Destroy')
+        booleanParam(name: 'TfDestroy', defaultValue: false, description: 'Terraform Destroy')
         string(name: 'WorkingDirectory', defaultValue: '$WORKSPACE', description: 'The working directory')
         string(name: 'TerraformVersion', defaultValue: 'latest', description: 'Terraform version')
-        booleanParam(name: "DebugMode", defaultValue: true, description: 'Enable DebugMode')
+        booleanParam(name: 'DebugMode', defaultValue: true, description: 'Enable DebugMode')
+        // Add more parameters as needed
     }
 
     environment {
         TFENV_AUTO_INSTALL = 'false'
+        ARM_TENANT_ID = credentials('SpokeTenantId')
+        ARM_SUBSCRIPTION_ID = credentials('SpokeSubId')
+        BACKEND_STORAGE_SUBSCRIPTION_ID = credentials('SpokeSubId')
+        BACKEND_STORAGE_RESOURCE_GROUP_NAME = credentials('SpokeSaRgName')
+        BACKEND_STORAGE_ACCOUNT_NAME = credentials('SpokeSaName')
+        BACKEND_STORAGE_ACCOUNT_BLOB_CONTAINER_NAME = credentials('SpokeSaBlobContainerName')
     }
 
     stages {
         stage('Run Script') {
             steps {
                 script {
-                    pwsh """
-                        pwsh -File Run-Terraform.ps1 `
-                        -WorkingDirectory ${{ inputs.working_directory }} `
-                        -RunTerraformInit ${{ inputs.run_terraform_init }} `
-                        -RunTerraformPlan ${{ inputs.run_terraform_plan }} `
-                        -RunTerraformPlanDestroy ${{ inputs.run_terraform_plan_destroy }} `
-                        -RunTerraformApply ${{ inputs.run_terraform_apply }} `
-                        -RunTerraformDestroy ${{ inputs.run_terraform_destroy }} `
-                        -DebugMode ${{ inputs.enable_debug_mode }} `
-                        -DeletePlanFiles ${{ inputs.delete_plan_files }} `
-                        -TerraformVersion ${{ inputs.terraform_version }} `
-                        -BackendStorageSubscriptionId ${{ secrets.SpokeSubId }} `
-                        -BackendStorageResourceGroupName ${{ secrets.SpokeSaRgName }} `
-                        -BackendStorageAccountName ${{ secrets.SpokeSaName }} `
-                        -BackendStorageAccountBlobContainerName ${{ secrets.SpokeSaBlobContainerName }} `
-                        -TerraformStateName ${{ inputs.terraform_state_name }}
-                    """
+                    withCredentials([usernamePassword(credentialsId: SvpDev, usernameVariable: 'ARM_CLIENT_ID', passwordVariable: 'ARM_CLIENT_SECRET')]) {
+                        pwsh """
+                            pwsh -File Run-Terraform.ps1 `
+                            -WorkingDirectory $params.WorkingDirectory `
+                            -RunTerraformInit $params.TfInit `
+                            -RunTerraformPlan $params.TfPlan `
+                            -RunTerraformPlanDestroy $params.TfPlanDestroy `
+                            -RunTerraformApply $params.TfApply `
+                            -RunTerraformDestroy $params.TfDestroy `
+                            -DebugMode $params.DebugMode `
+                            -DeletePlanFiles $params.DeletePlanFiles `
+                            -TerraformVersion $params.TerraformVersion `
+                            -BackendStorageSubscriptionId $env:BACKEND_STORAGE_SUBSCRIPTION_ID `
+                            -BackendStorageResourceGroupName $env:BACKEND_STORAGE_RESOURCE_GROUP_NAME `
+                            -BackendStorageAccountName $env:BACKEND_STORAGE_ACCOUNT_NAME `
+                            -BackendStorageAccountBlobContainerName $env:BACKEND_STORAGE_ACCOUNT_BLOB_CONTAINER_NAME `
+                            -TerraformStateName $params.TerraformStateName
+                        """
+                    }
                 }
             }
         }
